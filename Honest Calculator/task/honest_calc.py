c0382aab54f36b7dbfbb71efaeb259de4cd45314
feztix <pan0v.s.s@yandex.ru>
2023-02-19 19:56:39 +0300

    Stage 1/5: Stage 1 complete

--- Honest Calculator/task/honest_calc.py
+++ Honest Calculator/task/honest_calc.py
@@ -1,7 +1,30 @@
-# write your code here
 msg_0 = "Enter an equation"
-
 msg_1 = "Do you even know what numbers are? Stay focused!"
-
 msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
 
+
+def enter():
+    lexemes = ['+', '-', '/', '*']
+    result = 0
+    print(msg_0)
+    user_input = input().split()
+    try:
+        x = int(user_input[0])
+        operand = str(user_input[1])
+        y = float(user_input[2])
+        if operand not in lexemes:
+            print(msg_2)
+            enter()
+    except ValueError:
+        print(msg_1)
+        enter()
+    except SyntaxError:
+        print(msg_2)
+        enter()
+    except IndexError:
+        print("Man")
+        enter()
+
+
+if __name__ == '__main__':
+    enter()
