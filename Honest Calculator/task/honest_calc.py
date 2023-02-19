71f0f667715690907ba9bbfce34c0156d13a19fe
feztix <pan0v.s.s@yandex.ru>
2023-02-19 19:51:03 +0300

    Stage 1/5: Initial commit

---
+++ Honest Calculator/task/honest_calc.py
@@ -0,0 +1,7 @@
+# write your code here
+msg_0 = "Enter an equation"
+
+msg_1 = "Do you even know what numbers are? Stay focused!"
+
+msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
+
